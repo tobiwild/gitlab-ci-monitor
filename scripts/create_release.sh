efe18a3403cd653157e1ff37641b60c0a0d9a77c
Tobias Wild <tobiwild@web.de>
2017-08-06 17:37:40 +0200

    add script to create new release

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -1,17 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-# USAGE: docker run --rm -v $PWD:/workspace -v $PWD/rel:/output bitwalker/alpine-elixir /workspace/scripts/create_release.sh
-
-export MIX_ENV=prod
-cp -a /workspace /build
-cd /build
-
-mix do deps.get, compile
-# TODO: elm is not working in alpine container
-#npm install
-#npm run deploy
-mix phoenix.digest
-mix release --env=prod
-
-cp /build/_build/prod/rel/gitlab_ci_monitor/releases/0.0.1/gitlab_ci_monitor.tar.gz /output
+npm run deploy
+docker run --rm -v $PWD:/workspace -v $PWD/rel:/output bitwalker/alpine-elixir /workspace/scripts/create_release_docker.sh
+docker build -t tobiwild/gitlab-ci-monitor .
---
+++ scripts/create_release_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+export MIX_ENV=prod
+cp -a /workspace /build
+cd /build
+
+mix do deps.get, compile
+# TODO: elm is not working in alpine container
+#npm install
+#npm run deploy
+mix phoenix.digest
+mix release --env=prod
+
+cp /build/_build/prod/rel/gitlab_ci_monitor/releases/0.0.1/gitlab_ci_monitor.tar.gz /output
